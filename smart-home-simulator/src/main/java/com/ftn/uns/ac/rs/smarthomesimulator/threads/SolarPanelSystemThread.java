d7611e97e3be34223fd52a4a86a23354b7de4f14
Vuk Radmilović <vuk.radmilovic01@gmail.com>
2023-12-18 02:30:39 +0100

    Optimize solar panel power production formula

--- smart-home-simulator/src/main/java/com/ftn/uns/ac/rs/smarthomesimulator/threads/SolarPanelSystemThread.java
+++ smart-home-simulator/src/main/java/com/ftn/uns/ac/rs/smarthomesimulator/threads/SolarPanelSystemThread.java
@@ -1,12 +1,7 @@
 package com.ftn.uns.ac.rs.smarthomesimulator.threads;
 
 import com.ftn.uns.ac.rs.smarthomesimulator.config.MqttConfiguration;
-import com.ftn.uns.ac.rs.smarthomesimulator.models.ACCommand;
-import com.ftn.uns.ac.rs.smarthomesimulator.models.ACStateChange;
 import com.ftn.uns.ac.rs.smarthomesimulator.models.devices.SolarPanelSystem;
-import com.ftn.uns.ac.rs.smarthomesimulator.models.enums.ACState;
-import com.ftn.uns.ac.rs.smarthomesimulator.models.enums.CommandType;
-import com.ftn.uns.ac.rs.smarthomesimulator.services.MqttService;
 import com.ftn.uns.ac.rs.smarthomesimulator.services.ThreadMqttService;
 import org.eclipse.paho.mqttv5.client.IMqttToken;
 import org.eclipse.paho.mqttv5.client.MqttCallback;
@@ -20,18 +15,56 @@ import org.slf4j.LoggerFactory;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import java.util.HashMap;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
-import java.util.Map;
 
 public class SolarPanelSystemThread implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(SolarPanelSystemThread.class);
+    public static final int INTERVAL = 5;
     private final Integer deviceId;
     private final SolarPanelSystem system;
     private MqttConfiguration mqttConfiguration;
     private ThreadMqttService mqttService;
     private boolean isOff = true;
     private int count = 1;
+
+    private static final double[] sunlightStarts = {6.5, 6.5, 6, 5.5, 5, 5, 5, 5.5, 6, 7, 7, 6.5};
+    private static final double[] sunlightEnds = {16, 17, 17.5, 19, 20, 20, 20, 19.5, 19, 18, 17, 16};
+    private static final double SUNLIGHT_PEAK = 12;
+    private final double sunlightLengthToday;
+
+    private final double sunlightWhichWillShineToday = calculateSunWhichWillShineToday();
+
+    private double getSunlightIntensity() {
+        Date currentDate = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
+        int i = calendar.get(Calendar.MONTH);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+
+        double distanceFromPeak = Math.abs(hour - SUNLIGHT_PEAK);
+        double percentage = 1 - (distanceFromPeak / (SUNLIGHT_PEAK - sunlightStarts[i]));
+        return Math.max(0, percentage) / sunlightWhichWillShineToday;
+    }
+
+    private double getSunlightIntensity(int month, int hour) {
+        double distanceFromPeak = Math.abs(hour - SUNLIGHT_PEAK);
+        double percentage = 1 - (distanceFromPeak / (SUNLIGHT_PEAK - sunlightStarts[month]));
+        return Math.max(0, percentage);
+    }
+
+    private double calculateSunWhichWillShineToday() {
+        Date currentDate = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
+        int i = calendar.get(Calendar.MONTH);
+        double power = 0;
+        for (int j = 0; j < 24; j++) {
+            power += getSunlightIntensity(i, j);
+        }
+        return power;
+    }
 
     private class MqttSPSMessageCallback implements MqttCallback {
         @Override
@@ -68,6 +101,12 @@ public class SolarPanelSystemThread implements Runnable {
         }
         this.system = system;
         this.deviceId = system.getId();
+
+        Date currentDate = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
+        int i = calendar.get(Calendar.MONTH);
+        sunlightLengthToday = sunlightEnds[i] - sunlightStarts[i];
     }
 
     @Override
@@ -86,8 +125,14 @@ public class SolarPanelSystemThread implements Runnable {
     public void generatePower() throws InterruptedException {
         while (true) {
             sendInternalState();
-            sendAndDisplayPower(system.getNumberOfPanels() * system.getPanelSize() * system.getPanelEfficiency());
-            Thread.sleep(5 * 1000);
+            // kWh = (number of panels * panel size * panel efficiency * (sunlight length today / 3 `peak sun hours only`)
+            // amount of power which will be produced today
+            double kWhProduced = system.getNumberOfPanels() * system.getPanelSize() * system.getPanelEfficiency() * (sunlightLengthToday / 3);
+            // multiply by the percentage of today's power which will be generated during this hour
+            kWhProduced *= getSunlightIntensity(); // amount of power which will be produced in this hour
+            kWhProduced /= ((60.0 * 60) / INTERVAL); // amount of power which will be produced in this interval
+            sendAndDisplayPower(kWhProduced);
+            Thread.sleep(INTERVAL * 1000);
         }
     }
 
