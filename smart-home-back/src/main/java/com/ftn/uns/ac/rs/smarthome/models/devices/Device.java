67a89364fa1c4e68c88ee60671d52f8f58bc9ca4
Vuk Radmilović <vuk.radmilovic01@gmail.com>
2024-02-03 13:25:09 +0100

    remove unique constraint from device name

--- smart-home-back/src/main/java/com/ftn/uns/ac/rs/smarthome/models/devices/Device.java
+++ smart-home-back/src/main/java/com/ftn/uns/ac/rs/smarthome/models/devices/Device.java
@@ -24,7 +24,7 @@ public class Device {
     @JoinColumn(name = "property_id", referencedColumnName = "id", nullable = false)
     private Property property;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     private String name;
 
     @Column
