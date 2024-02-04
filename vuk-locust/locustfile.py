67863767227e112fd1e01caf75f7921cb67e9ea5
Vuk Radmilović <vuk.radmilovic01@gmail.com>
2024-02-04 07:11:02 +0100

    add vuk locustfile

---
+++ vuk-locust/locustfile.py
@@ -0,0 +1,123 @@
+from locust import HttpUser, task, between, tag
+import random, datetime
+
+
+class Test(HttpUser):
+    wait_time = between(1, 1)
+    host = "http://localhost:8080"
+    user_token = "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzbWFydC1ob21lIiwic3ViIjoidnVrIiwiYXVkIjoid2ViIiwiaWF0IjoxNzA3MDEyOTg1LCJleHAiOjE3MDkxNjA0NjksInJvbGUiOiJST0xFX1VTRVIifQ.ZQcW5XHcAw-Hd5W6a8_lu7nNIXjUuNjYhWg3xH1E0almlWFQbbYwFwBI6mSU3wZTKFFzzKj_dwc65NC5V-7fIA"
+    admin_token = "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzbWFydC1ob21lIiwic3ViIjoiYWRtaW4iLCJhdWQiOiJ3ZWIiLCJpYXQiOjE3MDcwMjE1MTgsImV4cCI6MTcwOTE2OTAwMiwicm9sZSI6IlJPTEVfU1VQRVJBRE1JTiJ9.x5OT_59DFTOBJ7OkVUly2rc8zqgpygxPnHrViXuYp35UuwnwoOv9r1pXU_i-hksb_lNnaJ5fVQjq79oFllq1dg"
+
+    @tag("scene2")
+    @task
+    def view_properties_admin(self):
+        headers = {"Authorization": f"Bearer {self.admin_token}"}
+        with self.client.get("/api/property/approvedProperties", headers=headers, catch_response=True, name="View properties") as response:
+            if response.status_code != 200:
+                response.failure("Failed to view properties")
+            else:
+                response.success()
+
+    @tag("scene3")
+    @task
+    def view_property_power_admin(self):
+        headers = {"Authorization": f"Bearer {self.admin_token}"}
+        now = datetime.datetime.utcnow()
+        start = now - datetime.timedelta(days=30)
+        with self.client.get(f"http://localhost/api/devices/measurements?from={int(start.timestamp())}&to={int(now.timestamp())}&deviceId=1"
+                             f"&measurement=totalProduction", headers=headers, catch_response=True,
+                             name="View property power production") as response:
+            if response.status_code != 200:
+                response.failure("Failed to view property power production")
+            else:
+                with self.client.get(
+                        f"http://localhost/api/devices/measurements?from={int(start.timestamp())}&to={int(now.timestamp())}&deviceId=1"
+                        f"&measurement=totalConsumption", headers=headers, catch_response=True,
+                        name="View property power consumption") as response:
+                    if response.status_code != 200:
+                        response.failure("Failed to view property power consumption")
+                    else:
+                        response.success()
+
+    @tag("scene4")
+    @task
+    def view_city_power_admin(self):
+        headers = {"Authorization": f"Bearer {self.admin_token}"}
+        now = datetime.datetime.utcnow()
+        start = now - datetime.timedelta(days=30)
+        with self.client.get(f"http://localhost/api/devices/powerMeasurements?from={int(start.timestamp())}&to={int(now.timestamp())}&cityId=1"
+                             f"&measurement=totalProduction", headers=headers, catch_response=True,
+                             name="View city power production") as response:
+            if response.status_code != 200:
+                response.failure("Failed to view city power production")
+            else:
+                with self.client.get(
+                        f"http://localhost/api/devices/powerMeasurements?from={int(start.timestamp())}&to={int(now.timestamp())}&cityId=1"
+                        f"&measurement=totalConsumption", headers=headers, catch_response=True,
+                        name="View city power consumption") as response:
+                    if response.status_code != 200:
+                        response.failure("Failed to view city power consumption")
+                    else:
+                        response.success()
+
+    @tag("scene5")
+    @task
+    def solar_panel_turn_on_off(self):
+        headers = {"Authorization": f"Bearer {self.user_token}"}
+        sps_id = random.randint(1, 2)
+        with self.client.put(f"/api/devices/sps/{sps_id}/off", headers=headers, catch_response=True, name="Turn off solar panel") as response:
+            if response.status_code != 200:
+                response.failure("Failed to turn off solar panel")
+            else:
+                with self.client.put(f"/api/devices/sps/{sps_id}/on", headers=headers, catch_response=True,
+                                     name="Turn on solar panel") as response:
+                    if response.status_code != 200:
+                        response.failure("Failed to turn on solar panel")
+                    else:
+                        response.success()
+
+    @tag("scene6")
+    @task
+    def view_solar_panel_commands(self):
+        headers = {"Authorization": f"Bearer {self.user_token}"}
+        sps_id = random.randint(1, 2)
+        with self.client.get(f"/api/devices/commands?from=0&to=9007199254740991&deviceId={sps_id}&page=0&size=100&firstFetch=true&userId=-1", headers=headers, catch_response=True, name="View solar panel commands") as response:
+            if response.status_code != 200:
+                response.failure("Failed to view solar panel commands")
+            else:
+                response.success()
+
+    @tag("scene7")
+    @task
+    def view_property_power_consumption(self):
+        headers = {"Authorization": f"Bearer {self.user_token}"}
+        now = datetime.datetime.utcnow()
+        start = now - datetime.timedelta(days=30)
+        with self.client.get(f"/api/devices/measurements?from={int(start.timestamp())}&to={int(now.timestamp())}"
+                             f"&deviceId=1&measurement=totalConsumption", headers=headers, catch_response=True,
+                             name="View property power consumption") as response:
+            if response.status_code != 200:
+                response.failure("Failed to view property power consumption")
+            else:
+                response.success()
+
+    @tag("scene8")
+    @task
+    def view_charger_commands(self):
+        headers = {"Authorization": f"Bearer {self.user_token}"}
+        charger_id = random.randint(3, 4)
+        with self.client.get(f"/api/devices/commands?from=0&to=9007199254740991&deviceId={charger_id}&page=0&size=100&firstFetch=true&userId=-1", headers=headers, catch_response=True, name="View charger commands") as response:
+            if response.status_code != 200:
+                response.failure("Failed to view charger commands")
+            else:
+                response.success()
+
+    @tag("scene10")
+    @task
+    def view_devices(self):
+        headers = {"Authorization": f"Bearer {self.user_token}"}
+        with self.client.get("/api/devices/ownerAll", headers=headers, catch_response=True, name="View devices") as response:
+            if response.status_code != 200:
+                response.failure("Failed to view devices")
+            else:
+                response.success()
